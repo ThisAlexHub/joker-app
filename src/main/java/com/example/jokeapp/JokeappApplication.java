5bc19a8e15a3cc1443284d6195e4bfbb29a2c048
thisalexhub <alex@saivian.net>
2021-01-14 21:34:39 -0800

    the app is done

--- src/main/java/com/example/jokeapp/JokeappApplication.java
+++ src/main/java/com/example/jokeapp/JokeappApplication.java
@@ -8,6 +8,9 @@ public class JokeappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JokeappApplication.class, args);
+
+
+        System.out.println("Hello");
     }
 
 }
